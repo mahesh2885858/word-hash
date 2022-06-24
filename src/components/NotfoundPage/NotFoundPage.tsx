ed4d6949077d8db55ccfc9fcd105eb0151db0a1b
mahesh <mahesh2885858@gmail.com>
2022-06-24 06:05:53 -0400

    small chnage

--- src/components/NotfoundPage/NotFoundPage.tsx
+++ src/components/NotfoundPage/NotFoundPage.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import "./notfoundpage.scss";
 const NotFoundPage: React.FC = () => {
   return (
-    <div className="notfound-div h-[calc(100vh-120px)] flex justify-center items-center text-center">
+    <div className="notfound-div h-[calc(100vh-120px)] flex flex-col justify-center items-center text-center">
       <p>The page you are looking for is not found</p>
       <p>
         Go to <Link to={`/`} className="text-xl text-blue-600"> Home Page</Link>
