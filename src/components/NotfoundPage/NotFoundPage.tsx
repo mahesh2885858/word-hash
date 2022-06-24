1cdc61e1268754c7f51e577d4dca1ef95dece1b5
mahesh <mahesh2885858@gmail.com>
2022-06-24 11:47:37 +0530

    added not found page styling

--- src/components/NotfoundPage/NotFoundPage.tsx
+++ src/components/NotfoundPage/NotFoundPage.tsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 import "./notfoundpage.scss";
 const NotFoundPage: React.FC = () => {
   return (
-    <div className="notfound-div">
+    <div className="notfound-div h-[calc(100vh-120px)] flex justify-center items-center text-center">
       <p>The page you are looking for is not found</p>
       <p>
-        Go to <Link to={`/`}> Home</Link>
+        Go to <Link to={`/`} className="text-xl text-blue-600"> Home Page</Link>
       </p>
     </div>
   );
